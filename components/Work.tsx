048fa83dde8a9e082f2be491bb4680ce867bd412
harshit Mishra <miananya2@gmail.com>
2024-04-16 06:41:18 +0530

    work images

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,13 +1,32 @@
+import { images } from "@/constants";
+import Image from "next/image";
+
 const Work = () => {
   return (
     <div className="w-full mt-6">
-      <div className="max-w-[1440px] mx-auto text-center">
+      <div className="relative max-w-[1440px] mx-auto text-center">
         <h1 className="text-[37vw] font-medium tracking-tight leading-none select-none">
           work
         </h1>
         <p className="text-zinc-400 text-lg font-regular leading-none text-muted">
           Web Design, Webflow Development, Creative Development
         </p>
+        <div className="absolute top-0 w-full h-full">
+          {images.map(
+            (image, index) =>
+              image.isActive && (
+                <Image
+                  src={image.url}
+                  alt="work"
+                  key={index}
+                  width={300}
+                  height={300}
+                  style={{ top: image.top, left: image.left }}
+                  className="absolute w-60 rounded-lg -translate-x-[50%] -translate-y-[50%]"
+                />
+              )
+          )}
+        </div>
       </div>
     </div>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -12,3 +12,42 @@ export const links = [
     href: "/news",
   },
 ];
+
+export const images = [
+  {
+    url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
+    top: "50%",
+    left: "50%",
+    isActive: false,
+  },
+  {
+    url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
+    top: "56%",
+    left: "44%",
+    isActive: false,
+  },
+  {
+    url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png",
+    top: "45%",
+    left: "56%",
+    isActive: false,
+  },
+  {
+    url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png",
+    top: "60%",
+    left: "53%",
+    isActive: false,
+  },
+  {
+    url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png",
+    top: "43%",
+    left: "40%",
+    isActive: false,
+  },
+  {
+    url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png",
+    top: "65%",
+    left: "55%",
+    isActive: true,
+  },
+];
