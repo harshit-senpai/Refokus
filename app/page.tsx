650a6a83a55299c5cc9517e6695f9d5cb8d3237a
harshit Mishra <miananya2@gmail.com>
2024-04-17 14:00:18 +0530

    using locomotive

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Work from "@/components/Work";
@@ -5,8 +6,21 @@ import Cards from "@/components/card/Cards";
 import Marquees from "@/components/marquee/Marquees";
 import Products from "@/components/products/Products";
 import Strips from "@/components/strips/Strips";
+import LocomotiveScroll from "locomotive-scroll";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    let scroll: import("locomotive-scroll");
+    import("locomotive-scroll").then((locomotiveModule) => {
+      scroll = new locomotiveModule.default();
+    });
+
+    // cleanup phase
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  });
   return (
     <div className="w-full bg-zinc-900 overflow-y-none">
       <Navbar />
