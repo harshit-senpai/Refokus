deb373c2d35dfe1abedf1fbf3ba22a7dec72540d
harshit Mishra <miananya2@gmail.com>
2024-04-18 02:37:43 +0530

    sliding images

--- components/products/Product.tsx
+++ components/products/Product.tsx
@@ -5,12 +5,26 @@ interface ProductProps {
   description: string;
   live: boolean;
   caseStudy?: boolean;
+  count: number;
+  mover: (value: number) => void;
 }
 
-const Product = ({ title, description, live, caseStudy }: ProductProps) => {
+const Product = ({
+  title,
+  description,
+  live,
+  caseStudy,
+  mover,
+  count,
+}: ProductProps) => {
   return (
-    <div className="w-full py-20 px-20 hover:bg-gradient-to-r from-indigo-600 via-purple-500 transition-all duration-3000 hover:py-28">
-      <div className="max-w-[1440px] mx-auto flex items-center justify-between">
+    <div className="w-full h-[20rem] py-10 px-20 hover:bg-gradient-to-r from-indigo-600 via-purple-500 transition-all duration-3000">
+      <div
+        onMouseEnter={() => {
+          mover(count);
+        }}
+        className="max-w-[1440px] mx-auto flex items-center justify-between"
+      >
         <h1 className="text-6xl font-medium capitalize">{title}</h1>
         <div className="w-1/3">
           <p className="mb-10 text-lg font-regular text-muted">{description}</p>
--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -1,9 +1,19 @@
+"use client";
 import { products } from "@/constants";
 import Product from "./Product";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Image from "next/image";
 
 const Products = () => {
+  const [position, setPosition] = useState(46);
+
+  const moverHandler = (value: number) => {
+    setPosition(value * 20);
+  };
+
   return (
-    <div className="mt-20">
+    <div className="mt-20 relative">
       {products.map((product, index) => (
         <Product
           title={product.title}
@@ -11,8 +21,110 @@ const Products = () => {
           description={product.description}
           live={product.live}
           caseStudy={product.caseStudy}
+          mover={moverHandler}
+          count={index}
         />
       ))}
+      <div className="absolute top-0 w-full h-full pointer-events-none">
+        <motion.div
+          initial={{ y: position }}
+          animate={{ y: position + `rem` }}
+          transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.6 }}
+          className="absolute left-1/3 w-[26rem] h-[20rem] bg-white overflow-hidden rounded-lg"
+        >
+          <motion.div
+            animate={{ y: -position + `rem` }}
+            transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.5 }}
+            className="w-full h-full rounded-lg bg-sky-100"
+          >
+            <Image
+              src="https://assets-global.website-files.com/6334198f239547f2fccd84c1/650c5f15978b05dc7f2ae554_14.webp"
+              height={4200}
+              width={4200}
+              className="h-full w-full"
+              alt="showcase"
+            />
+          </motion.div>
+          <motion.div
+            animate={{ y: -position + `rem` }}
+            transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.5 }}
+            className="w-full h-full rounded-lg bg-sky-200"
+          >
+            <Image
+              src="https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b41324914b470b20ec7d03_Frame%203.png "
+              height={4200}
+              width={4200}
+              className="h-full w-full"
+              alt="showcase"
+            />
+          </motion.div>
+          <motion.div
+            animate={{ y: -position + `rem` }}
+            transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.5 }}
+            className="w-full h-full rounded-lg bg-sky-300"
+          >
+            <Image
+              src="https://assets-global.website-files.com/6334198f239547f2fccd84c1/65f1160e50447ddfb490fb59_portfolioCard_cc-02%20(1).webp"
+              height={4200}
+              width={4200}
+              className="h-full w-full"
+              alt="showcase"
+            />
+          </motion.div>
+          <motion.div
+            animate={{ y: -position + `rem` }}
+            transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.5 }}
+            className="w-full h-full rounded-lg bg-sky-400"
+          >
+            <Image
+              src="https://assets-global.website-files.com/6334198f239547f2fccd84c1/64cd6ecc885579ba1b4ffc97_10.jpg"
+              height={4200}
+              width={4200}
+              className="h-full w-full"
+              alt="showcase"
+            />
+          </motion.div>
+          <motion.div
+            animate={{ y: -position + `rem` }}
+            transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.5 }}
+            className="w-full h-full rounded-lg bg-sky-500"
+          >
+            <Image
+              src="https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b2cab86e8df24e528c4441_Frame%201.png"
+              height={4200}
+              width={4200}
+              className="h-full w-full"
+              alt="showcase"
+            />
+          </motion.div>
+          <motion.div
+            animate={{ y: -position + `rem` }}
+            transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.5 }}
+            className="w-full h-full rounded-lg bg-sky-600"
+          >
+            <Image
+              src="https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b41cfab581d98facecc35d_YIR2022%20-%20mobile.png"
+              height={4200}
+              width={4200}
+              className="h-full w-full"
+              alt="showcase"
+            />
+          </motion.div>
+          <motion.div
+            animate={{ y: -position + `rem` }}
+            transition={{ ease: [0.83, 0, 0.17, 1], duration: 0.5 }}
+            className="w-full h-full rounded-lg bg-sky-700"
+          >
+            <Image
+              src="https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b41cd5ec5fbe7d560d053f_Yahoo%20-%20mobile.png"
+              height={4200}
+              width={4200}
+              className="h-full w-full"
+              alt="showcase"
+            />
+          </motion.div>
+        </motion.div>
+      </div>
     </div>
   );
 };
